bf7944b2f44ef83663a1eb08adc2194704186b6e
SerhiiKlymchuk <klymchuk.buisness@gmail.com>
2021-01-05 18:29:47 +0200

    Initial commit

---
+++ src/main/java/com/serhiiklymchuk/gmailpet/GmailPetApplication.java
@@ -0,0 +1,14 @@
+package com.serhiiklymchuk.gmailpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailPetApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GmailPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serhiiklymchuk/gmailpet/GmailPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhiiklymchuk.gmailpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
